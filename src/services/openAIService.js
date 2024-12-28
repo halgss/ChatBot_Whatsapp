c6098f42d36364f98e709ea817ed71567f86d201
Henry Alcaraz <halgss@gmail.com>
2024-12-27 19:46:53 -0600

    Clase 22

--- src/services/openAIService.js
+++ src/services/openAIService.js
@@ -8,9 +8,12 @@ const client = new OpenAI({
 const openAIService = async (message) => {
     try{
         const response = await client.chat.completions.create({
-            messages: [{ role: 'system', content: 'promp'}, { role: 'user', content: message }], model: 'gpt-4o'
-        })
+            messages: [{ role: 'system', content: 'Comportarte como un veterinario, deberás de resolver las preguntas lo más simple posible. Responde en texto plano, como si fuera una conversación por WhatsApp, no saludes, no generas conversaciones, solo respondes con la pregunta del usuario.'}, { role: 'user', content: message }], model: 'gpt-4o'
+        });
+        return response.choices[0].message.content;
     } catch (error) {
         console.error(error);
     }
 }
+
+export default openAIService;
